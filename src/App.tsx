cca5b50a78aa97621d8385d5094c453c6884101a
Warren <w.n.bates@gmail.com>
2020-07-13 20:07:47 +0100

    Add type recognition to return value Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,46 @@
-import React from "react";
-import "./App.css";
-import * as FormUp from "./FormUp";
+import React from "react"
+import "./App.css"
+import * as FormUp from "./FormUp"
 import * as Yup from "yup"
 
 function App() {
-  const { fields, form } = FormUp.useFormUp({
-      name: FormUp.text(""),
-      email: FormUp.email(""),
-      dob: FormUp.date(""),
-    }, {
-      validationSchema: Yup.object().shape({
-        name: Yup.string().test('len', 'Must be exactly 5 characters', val => val.length === 5),
-        dob: Yup.date().required()
-      }),
-      onSubmit: (e) => console.log("Submitted"),
-    }
-  );
+	const { fields, form } = FormUp.useFormUp(
+		{
+			name: FormUp.text(""),
+			email: FormUp.email(""),
+			dob: FormUp.date(""),
+		},
+		{
+			validationSchema: Yup.object().shape({
+				name: Yup.string().test("len", "Must be exactly 5 characters", (val) => val.length === 5),
+				dob: Yup.date().required(),
+			}),
+			onSubmit: (e) => console.log("Submitted"),
+		}
+	)
 
-  return (
-    <div className="App">
-      <div>
-        <FormUp.Form form={form}>
-          <FormUp.Input field={fields.name} />
-          <FormUp.Input field={fields.email} />
-          <FormUp.Input field={fields.dob} />
-          <button type="submit">Submit</button>
-        </FormUp.Form>
-      </div>
-      <div>
-        <p>
-          The name is {fields.name.value}. Email is {fields.email.value}
-        </p>
-      </div>
-      <div>
-        <ul>
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div>
+				<FormUp.Form form={form}>
+					<FormUp.Input field={fields.name} />
+					<FormUp.Input field={fields.email} />
+					<FormUp.Input field={fields.dob} />
+					<button type="submit">Submit</button>
+				</FormUp.Form>
+			</div>
+      <button onClick={() => {fields.name.setValue(fields.name.value + "a")}}>Add "a" to name</button>
+      <button onClick={() => {fields.name.setTouched(!fields.name.touched)}}>Toggle name touched</button>
+			<div>
+				<p>
+					The name is {fields.name.value}. Email is {fields.email.value}
+				</p>
+			</div>
+			<div>
+				<ul></ul>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/FormUp.tsx
+++ src/FormUp.tsx
@@ -1,191 +1,216 @@
-import React, { useReducer, useState } from "react";
-import * as Yup from "yup";
+import React, { useReducer, useState } from "react"
+import * as Yup from "yup"
 
 type FieldDescriptor = {
-  type: string;
-  initial: any;
-};
+	type: string
+	initial: any
+}
 
 export function text(initial: any): FieldDescriptor {
-  return {
-    type: "text",
-    initial: initial,
-  };
+	return {
+		type: "text",
+		initial: initial,
+	}
 }
 
 export function email(initial: any): FieldDescriptor {
-  return {
-    type: "email",
-    initial: initial,
-  };
+	return {
+		type: "email",
+		initial: initial,
+	}
 }
 
 export function date(initial: any): FieldDescriptor {
-  return {
-    type: "date",
-    initial: initial,
-  };
+	return {
+		type: "date",
+		initial: initial,
+	}
+}
+export type FormDescriptor<T> = {
+	[K in keyof T]: FieldDescriptor
 }
 
-export type FormDescriptor = {
-  [fieldName: string]: FieldDescriptor;
-};
-
 export type Field = {
-  name: string;
-  type: string;
-  value: any;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  onBlur: (e: React.FocusEvent<HTMLInputElement>) => void
-  error?: string
-  touched: boolean
-};
+	name: string
+	type: string
+	value: any
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+	onBlur: (e: React.FocusEvent<HTMLInputElement>) => void
+	error?: string
+	touched: boolean
+	setValue: (value: any) => void | Promise<void>
+	setTouched: (touched: boolean) => void
+}
 
-export type Fields = {
-  [fieldName: string]: Field;
-};
+export type Fields<T> = {
+	[K in keyof T]: Field
+}
 
 export type FieldValues = {
-  [fieldName: string]: any;
-};
+	[fieldName: string]: any
+}
 
 export type FieldValueUpdateAction = {
-  fieldName: string;
-  value: any;
-};
+	fieldName: string
+	value: any
+}
 
 export type FormOptions = {
-  validationSchema?: Yup.ObjectSchema<object | undefined>;
-  onSubmit: (e: React.FormEvent) => void | Promise<any>;
-};
+	validationSchema?: Yup.ObjectSchema<object | undefined>
+	onSubmit: (e: React.FormEvent) => void | Promise<any>
+}
 
 export type FormSettings = {
-  onSubmit: (e: React.FormEvent) => void | Promise<any>;
-};
+	onSubmit: (e: React.FormEvent) => void | Promise<any>
+}
 export type FormDetails = {
-  validationErrors: Yup.ValidationError | undefined;
-};
+	validationErrors: Yup.ValidationError | undefined
+}
 
-const convertYupValidationError = (ex: Yup.ValidationError) => ex.inner.reduce((all, curr) => {
-  all[curr.path] = curr.message
-  return all
-}, {} as any)
+const convertYupValidationError = (ex: Yup.ValidationError) =>
+	ex.inner.reduce((all, curr) => {
+		all[curr.path] = curr.message
+		return all
+	}, {} as any)
 
-export function useFormUp(
-  formDescriptor: FormDescriptor,
-  options: FormOptions
-) {
-  const [validationErrors, setValidationErrors] = useState(
-    {} as any);
+export function useFormUp<T>(formDescriptor: FormDescriptor<T>, options: FormOptions) {
+	const [validationErrors, setValidationErrors] = useState({} as any)
 
-  const fieldNames = Object.keys(formDescriptor);
-  const initialValues = fieldNames.reduce((all, currentFieldName) => {
-    const currentFieldDescriptor = formDescriptor[currentFieldName];
-    all[currentFieldName] = currentFieldDescriptor.initial;
-    return all;
-  }, {} as FieldValues);
+	const fieldNames = Object.keys(formDescriptor)
 
-  const fieldValueChangeReducer = (state: FieldValues, action: FieldValueUpdateAction) => {
-    return {
-      ...state,
-      [action.fieldName]: action.value,
-    };
-  };
+	const initialValues = fieldNames.reduce((all, currentFieldName) => {
+		const currentFieldDescriptor = (formDescriptor as any)[currentFieldName] as FieldDescriptor
+		all[currentFieldName] = currentFieldDescriptor.initial
+		return all
+	}, {} as FieldValues)
 
-  const [fieldValues, dispatchFieldValueChange] = useReducer(fieldValueChangeReducer, initialValues);
+	const fieldValueChangeReducer = (state: FieldValues, action: FieldValueUpdateAction) => {
+		return {
+			...state,
+			[action.fieldName]: action.value,
+		}
+	}
 
-  const touchedReducer = (state: FieldValues, fieldName: string) => {
-    return {
-      ...state,
-      [fieldName]: true,
-    };
-  };
-  const [touched, dispatchTouchedChange] = useReducer(touchedReducer, fieldNames.reduce((prev, curr) => {
-    prev[curr] = false
-    return prev
-  }, {} as any))
+	const [fieldValues, dispatchFieldValueChange] = useReducer(fieldValueChangeReducer, initialValues)
 
-  const fields = fieldNames.reduce((all, currentFieldName) => {
-    const currentFieldDescriptor = formDescriptor[currentFieldName];
+	const touchedReducer = (state: FieldValues, action: { fieldName: string; touched: boolean }) => {
+		return {
+			...state,
+			[action.fieldName]: action.touched,
+		}
+	}
+	const [touched, dispatchTouchedChange] = useReducer(
+		touchedReducer,
+		fieldNames.reduce((prev, curr) => {
+			prev[curr] = false
+			return prev
+		}, {} as any)
+	)
 
-    all[currentFieldName] = {
-      name: currentFieldName,
-      type: currentFieldDescriptor.type,
-      value: fieldValues[currentFieldName],
-      onChange: (e: React.ChangeEvent<HTMLInputElement>) =>
-      dispatchFieldValueChange({ fieldName: currentFieldName, value: e.target.value }),
-      onBlur: (e: React.FocusEvent<HTMLInputElement>) => dispatchTouchedChange(currentFieldName),
-        error: validationErrors[currentFieldName],
-        touched: touched[currentFieldName]
-    };
-    return all;
-  }, {} as Fields);
+	const fields = fieldNames.reduce((all, currentFieldName) => {
+		const currentFieldDescriptor = (formDescriptor as any)[currentFieldName] as FieldDescriptor
 
-  const onSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+		all[currentFieldName] = {
+			name: currentFieldName,
+			type: currentFieldDescriptor.type,
+			value: fieldValues[currentFieldName],
+			onChange: (e: React.ChangeEvent<HTMLInputElement>) =>
+				dispatchFieldValueChange({
+					fieldName: currentFieldName,
+					value: e.target.value,
+				}),
+			onBlur: (e: React.FocusEvent<HTMLInputElement>) =>
+				dispatchTouchedChange({
+					fieldName: currentFieldName,
+					touched: true,
+				}),
+			error: validationErrors[currentFieldName],
+			touched: touched[currentFieldName],
+			setValue: (value: any) =>
+				dispatchFieldValueChange({
+					fieldName: currentFieldName,
+					value: value,
+				}),
+			setTouched: (touched: boolean) =>
+				dispatchTouchedChange({
+					fieldName: currentFieldName,
+					touched: touched,
+				}),
+		}
+		return all
+	}, {} as any) as Fields<T>
 
-    if (options.validationSchema) {
-      try {
-        await options.validationSchema.validate(fieldValues, {
-          abortEarly: false,
-        });
-        setValidationErrors({});
-      } catch (ex) {
-        const validationErrors = ex as Yup.ValidationError;
-        const convertedErrors = convertYupValidationError(validationErrors)
-        setValidationErrors(convertedErrors);
-        return false
-      }
-    }
+	const onSubmit = async (e: React.FormEvent) => {
+		e.preventDefault()
 
-    return options.onSubmit(e);
-  };
+		if (options.validationSchema) {
+			try {
+				await options.validationSchema.validate(fieldValues, {
+					abortEarly: false,
+				})
+				setValidationErrors({})
+			} catch (ex) {
+				const validationErrors = ex as Yup.ValidationError
+				const convertedErrors = convertYupValidationError(validationErrors)
+				setValidationErrors(convertedErrors)
+				return false
+			}
+		}
 
-  const form: FormSettings = {
-    onSubmit,
-  };
+		return options.onSubmit(e)
+	}
 
-  const details: FormDetails = {
-    validationErrors,
-  };
+	const form: FormSettings = {
+		onSubmit,
+	}
 
-  return { fields, form, details };
+	return { fields, form }
 }
 
 export interface InputProps extends React.HTMLAttributes<HTMLInputElement> {
-  field: Field;
+	field: Field
 }
 
 export function Input({ field, ...props }: InputProps) {
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    field.onChange(e)
-    if (props.onChange) {
-      props.onChange(e)
-    }
-  }
+	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		field.onChange(e)
+		if (props.onChange) {
+			props.onChange(e)
+		}
+	}
 
-  const onBlur = (e: React.FocusEvent<HTMLInputElement>) => {
-    field.onBlur(e)
-    if (props.onBlur) {
-      props.onBlur(e)
-    }
-  }
+	const onBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+		field.onBlur(e)
+		if (props.onBlur) {
+			props.onBlur(e)
+		}
+	}
 
-  const classes = props.className?.split(" ") || []
-  if (field.touched) {
-    classes.push("formup-touched")
-  }
-  if (field.error) {
-    classes.push("formup-error")
-  }
+	const classes = props.className?.split(" ") || []
+	if (field.touched) {
+		classes.push("formup-touched")
+	}
+	if (field.error) {
+		classes.push("formup-error")
+	}
 
-  return <input {...props} type={field.type} name={field.name} value={field.value} className={classes.join(" ")} onBlur={onBlur} onChange={onChange} />;
+	return (
+		<input
+			{...props}
+			type={field.type}
+			name={field.name}
+			value={field.value}
+			className={classes.join(" ")}
+			onBlur={onBlur}
+			onChange={onChange}
+		/>
+	)
 }
 
 export interface FormProps extends React.HTMLAttributes<HTMLFormElement> {
-  form: FormSettings;
+	form: FormSettings
 }
 
 export function Form({ form, ...props }: FormProps) {
-  return <form {...props} {...form} />;
+	return <form {...props} {...form} />
 }
